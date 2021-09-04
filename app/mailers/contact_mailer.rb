5fdb0457a3e82433bad561f6ecd0c49a38653bc8
Orizin <nlgporizin10@gmail.com>
2021-09-05 00:44:57 +0900

    お問い合わせ用のメイラーを設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,19 @@
 class ContactMailer < ApplicationMailer
-  def user_email(name:, email:)
-    @name = name
-    mail(
-      to: email,
-      subject: '【鍛高譚】お問い合わせを受付いたしました'
-    )
+  ADMIN_EMAIL = 'nlgporigin100@gmail.com'
+
+  def user_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = '【アプリ名】お問い合わせを受付いたしました'
+
+    mail(to: contact.email, subject: subject)
   end
 
-  def admin_email; end
+  def admin_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = '【アプリ名】お問い合わせがありました'
+
+    mail(to: ADMIN_EMAIL, subject: subject)
+  end
 end
